7bfc86ded90f49666518813704fd983a7183ad2d
amisukanta <amisukanta02@gmail.com>
2014-08-27 23:46:14 +0200

    prime factor with basic TDD environment

---
+++ PrimeFactor/prime_factor.rb
@@ -0,0 +1,2 @@
+class PrimeFactor
+end
---
+++ PrimeFactor/spec.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative 'prime_factor'
+
+
+describe "PrimeFactor" do 
+it 'exists' do
+
+	refute PrimeFactor.new
+ end
+end
