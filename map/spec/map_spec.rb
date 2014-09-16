dc44dc35d1eecd54d2a98697e1b5a9b554edd3c9
amisukanta <amisukanta02@gmail.com>
2014-09-16 17:49:47 +0200

    Added a failing test

--- map/spec/map_spec.rb
+++ map/spec/map_spec.rb
@@ -1,7 +1,7 @@
 describe 'Map' do
 
   it 'should iterate over array and retrun new array based on some addition' do
-    [1,2,3].map_insane {|e| e+1} eq [2,3,4]
+    expect([1,2,3].map_insane {|e| e+1}).eq [2,3,4]
   end
 
 end
