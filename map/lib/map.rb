8d68c195ea94c1be311446c61c8a55164ef127a8
Sukanta <amisukanta02@gmail.com>
2014-09-17 10:38:42 +0200

    indent correected

--- map/lib/map.rb
+++ map/lib/map.rb
@@ -1,12 +1,11 @@
 class Array
  
- def map_insane(&block)
-   return self.each unless block_given?
-   response = []
-   self.each do |e|
-     response.push yield(e) 
+  def map_insane(&block)
+    return self.each unless block_given?
+    response = []
+    self.each do |e|
+      response.push yield(e) 
     end 
-    response  
- end
- 	
+      response  
+  end
 end
