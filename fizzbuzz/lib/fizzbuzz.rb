d2a73dba64a6a9a80fde77a8b440d39aab0e3246
amisukanta <amisukanta02@gmail.com>
2014-09-23 17:02:41 +0200

    corrected the params value in fizzbuzz method

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -1,24 +1,24 @@
 
 
-	def fizzbuzz(number)
-	  return 'Fizz' if is_divisible_by_three?(3)
-	  return 'Buzz' if is_divisible_by_five?(5)
-	  return 'FizzBuzz' if is_divisible_by_fifteen?(15)
-	  number
-	end
+def fizzbuzz(number)
+  return 'Fizz' if is_divisible_by_three?(number)
+  return 'Buzz' if is_divisible_by_five?(number)
+  return 'FizzBuzz' if is_divisible_by_fifteen?(number)
+  number
+end
 
-	def is_divisible_by_three?(number)
-	  is_divisible_by(number, 3)
-	end
+def is_divisible_by_three?(number)
+  is_divisible_by(number, 3)
+end
 
-	def is_divisible_by_five?(number)
-	  is_divisible_by(number, 5)
-	end
+def is_divisible_by_five?(number)
+  is_divisible_by(number, 5)
+end
 
-	def is_divisible_by_fifteen?(number)
-	  is_divisible_by(number, 15)
-    end
+def is_divisible_by_fifteen?(number)
+  is_divisible_by(number, 15)
+end
 
-    def is_divisible_by(number, divisor)
-      number % divisor == 0
-    end
+def is_divisible_by(number, divisor)
+  number % divisor == 0
+end
