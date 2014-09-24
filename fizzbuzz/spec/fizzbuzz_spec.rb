179920bc10875f3a1816c9a1e00b11d6963a3830
amisukanta <amisukanta02@gmail.com>
2014-09-24 13:21:00 +0200

    Spec test successfull

--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -1,28 +1,31 @@
 require 'fizzbuzz'
 
 describe 'FizzBuzz' do 
+  
   context 'Knows that a number is divisible by' do 
     it '3' do
-      expect(is_divisible_by_three?(3)).to be_true
+      expect(is_divisible_by_three?(3)).to be true
     end
     it '5' do
-      expect(is_divisible_by_five?(5)).to be_true
+      expect(is_divisible_by_five?(5)).to be true
     end
     it '15' do
-      expect(is_divisible_by_fifteen?(15)).to be_true
+      expect(is_divisible_by_fifteen?(15)).to be true
     end
   end
+  
   context 'Knows that numbers are not divisible by' do
     it '3' do
-      expect(is_divisible_by_three?(3)).to be_false
+      expect(is_divisible_by_three?(1)).to be false
     end
     it '5' do
-      expect(is_divisible_by_five?(5)).to be_false
+      expect(is_divisible_by_five?(1)).to be false
     end
     it '15' do
-      expect(is_divisible_by_fifteen?(15)).to be_false
+      expect(is_divisible_by_fifteen?(1)).to be false
     end
   end
+  
   context 'when palying the game retrun' do
     it 'number' do
       expect(fizzbuzz(1)).to eq 1
