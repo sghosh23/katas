8b652f8ee6afb95eb8ae91535f63753309df3be6
amisukanta <amisukanta02@gmail.com>
2014-09-26 17:47:36 +0200

    Encapsulate methods in class and rspec test passed

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -1,24 +1,25 @@
+# calcutaling fizzbuzz through numbers
+class FizzBuzz
+  def fizzbuzz(number)
+	  return 'FizzBuzz' if divisible_by_fifteen?(number)	
+	  return 'Buzz' if divisible_by_five?(number)
+	  return 'Fizz' if divisible_by_three?(number)
+	  number
+	end
 
+	def divisible_by_three?(number)
+	  divisible_by(number, 3)
+	end
 
-def fizzbuzz(number)
-  return 'FizzBuzz' if is_divisible_by_fifteen?(number)	
-  return 'Buzz' if is_divisible_by_five?(number)
-  return 'Fizz' if is_divisible_by_three?(number)
-  number
-end
+	def divisible_by_five?(number)
+	  divisible_by(number, 5)
+	end
 
-def is_divisible_by_three?(number)
-  is_divisible_by(number, 3)
-end
+	def divisible_by_fifteen?(number)
+	  divisible_by(number, 15)
+	end
 
-def is_divisible_by_five?(number)
-  is_divisible_by(number, 5)
-end
-
-def is_divisible_by_fifteen?(number)
-  is_divisible_by(number, 15)
-end
-
-def is_divisible_by(number, divisor)
-  number % divisor == 0
+	def divisible_by(number, divisor)
+	  number % divisor == 0
+	end
 end
--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -1,43 +1,41 @@
 require 'fizzbuzz'
 
-describe 'FizzBuzz' do 
-  
-  context 'Knows that a number is divisible by' do 
+describe '#FizzBuzz' do
+  let(:fizzbuzz) { FizzBuzz.new }
+  context 'Knows that a number is divisible by' do
     it '3' do
-      expect(is_divisible_by_three?(3)).to be true
+      expect(fizzbuzz.divisible_by_three?(3)).to be true
     end
     it '5' do
-      expect(is_divisible_by_five?(5)).to be true
+      expect(fizzbuzz.divisible_by_five?(5)).to be true
     end
     it '15' do
-      expect(is_divisible_by_fifteen?(15)).to be true
+      expect(fizzbuzz.divisible_by_fifteen?(15)).to be true
     end
   end
-  
   context 'Knows that numbers are not divisible by' do
     it '3' do
-      expect(is_divisible_by_three?(1)).to be false
+      expect(fizzbuzz.divisible_by_three?(1)).to be false
     end
     it '5' do
-      expect(is_divisible_by_five?(1)).to be false
+      expect(fizzbuzz.divisible_by_five?(1)).to be false
     end
     it '15' do
-      expect(is_divisible_by_fifteen?(1)).to be false
+      expect(fizzbuzz.divisible_by_fifteen?(1)).to be false
     end
   end
-  
   context 'when palying the game retrun' do
     it 'number' do
-      expect(fizzbuzz(1)).to eq 1
+      expect(fizzbuzz.fizzbuzz(1)).to eq 1
     end
     it 'Fizz' do
-      expect(fizzbuzz(3)).to eq "Fizz"
+      expect(fizzbuzz.fizzbuzz(3)).to eq 'Fizz'
     end
     it 'Buzz' do
-      expect(fizzbuzz(5)).to eq "Buzz"
+      expect(fizzbuzz.fizzbuzz(5)).to eq 'Buzz'
     end
     it 'FizzBuzz' do
-      expect(fizzbuzz(15)).to eq "FizzBuzz"
+      expect(fizzbuzz.fizzbuzz(15)).to eq 'FizzBuzz'
     end
   end
 end
